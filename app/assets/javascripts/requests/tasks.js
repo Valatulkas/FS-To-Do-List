fb68832cf587fdc634b4e874e965c62f1b8eecb6
jmferg11 <jfergie93@gmail.com>
2021-10-18 22:04:25 -0400

    initial commit - troubleshooting jquery connectivity

--- app/assets/javascripts/requests/tasks.js
+++ app/assets/javascripts/requests/tasks.js
@@ -2,9 +2,9 @@ $.ajaxSetup({
     headers: {
       'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
     }
-  });
+});
   
-  var indexTasks = function (successCB, errorCB) {
+var indexTasks = function (successCB, errorCB) {
     var request = {
       type: 'GET',
       url: 'api/tasks?api_key=1',
@@ -13,9 +13,9 @@ $.ajaxSetup({
     }
   
     $.ajax(request);
-  };
+};
   
-  var postTask = function (content, successCB, errorCB) {
+var postTask = function (content, successCB, errorCB) {
     var request = {
       type: 'POST',
       url: 'api/tasks?api_key=1',
@@ -29,4 +29,49 @@ $.ajaxSetup({
     }
   
     $.ajax(request);
+};
+
+var deleteTask = function (id) {
+  var request = {
+    type: 'DELETE',
+    url: 'api/tasks/:' + id + '?api_key=1',
+    success: function (response, textStatus) {
+      indexTasks();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
   };
+  $.ajax(request);
+}  
+
+var markComplete = function(id) {
+  var request = {
+    type: 'PUT',
+    url: 'api/tasks/:' + id + '/mark_complete?api_key=1',
+    success: function (response, textStatus) {
+      indexTasks();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  };
+  $.ajax(request);
+}
+
+var markActive = function(id) {
+  var request = {
+    type: 'PUT',
+    url: 'api/tasks/:' + id + '/mark_active?api_key=1',
+    success: function (response, textStatus) {
+      indexTasks();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  };
+  $.ajax(request);
+}
+
+
+
--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -2,8 +2,12 @@ $(document).on("turbolinks:load", function () {
     if ($('.static_pages.index').length > 0) {
       indexTasks(function (response) {
         var htmlString = response.tasks.map(function(task) {
-          return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
-            " + task.content + "\
+          return "<div class='col-12 my-3 p-2 border rounded text-center'>\
+            <div class='task' data-id='" + task.id + "'> \
+            " + task.content + " </div>\
+            <button class='delete rounded' data-id='" + task.id + "'>Delete</button>\
+            <input type='checkbox' class='mark-complete ml-3'\
+            data-id='" + task.id + "' '+ (task.completed ? 'checked' : '') + >\
             </div>";
         });
   
@@ -11,3 +15,25 @@ $(document).on("turbolinks:load", function () {
       });
     }
   });
+
+$(document).on('#create-task').submit(function (event) {
+  event.preventDefault();
+  postTask(function () {
+    return "<div class='col-12 mb-3 p-2 border rounded task' data-id='"
+     + task.id + "'> \
+    " + content + "\
+      </div>";
+  })
+});
+
+$(document).on('click', '.delete', function () {
+  deleteTask($(this).data('id'));
+});
+
+$(document).on('change', '.mark-complete', function () {
+  if (this.checked) {
+    markComplete($(this).data('id'));
+  } else {
+    markActive($(this).data('id'));
+  }
+})
