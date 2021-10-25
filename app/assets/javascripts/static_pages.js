a97dfea11f52f644ac1d75a57372079456b744f8
jmferg11 <jfergie93@gmail.com>
2021-10-25 09:20:57 -0400

    add mark complete and mark active persistence, adjust button color

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,30 +1,19 @@
 $(document).on("turbolinks:load", function () {
     if ($('.static_pages.index').length > 0) {
-      indexTasks(function (response) {
-        var htmlString = response.tasks.map(function(task) {
-          $('#new-task-content').empty();
-          return "<div class='col-12 my-3 p-2 border rounded text-center'>\
-            <div class='task' data-id='" + task.id + "'> \
-            " + task.content + " </div>\
-            <button class='delete rounded' data-id='" + task.id + "'>Delete</button>\
-            <input type='checkbox' class='mark-complete ml-3'\
-            data-id='" + task.id + "' '+ (task.completed ? 'checked' : '') + >\
-            </div>";
-        });
-        $("#tasks").html(htmlString);
-      });
+      indexTasks(injectTasksIntoDom);
     }
 });
 
 var injectTasksIntoDom = function (response) {
+  console.log(response)
   var htmlString = response.tasks.map(function(task) {
     $('#new-task-content').empty();
-    return "<div class='col-12 my-3 p-2 border rounded text-center'>\
+    return "<div class='col-12 my-3 p-2 border rounded text-center tasks'>\
       <div class='task' data-id='" + task.id + "'> \
       " + task.content + " </div>\
-      <button class='delete rounded' data-id='" + task.id + "'>Delete</button>\
+      <button class='delete rounded btn-danger' data-id='" + task.id + "'>Delete</button>\
       <input type='checkbox' class='mark-complete ml-3'\
-      data-id='" + task.id + "' '+ (task.completed ? 'checked' : '') + >\
+      data-id='" + task.id + "' "+ (task.completed ? 'checked' : '') + ">\
       </div>";
   });
    $("#tasks").html(htmlString);
@@ -32,18 +21,17 @@ var injectTasksIntoDom = function (response) {
 
 $(document).on('#create-task').submit(function (event) {
   event.preventDefault();
-  postTask(function () {
+  postTask(function (data) {
     $('#new-task-content').empty();
     return "<div class='col-12 mb-3 p-2 border rounded task' data-id='"
-     + task.id + "'> \
-    " + content + "\
+     + data.task.id + "'> \
+    " + data.content + "\
       </div>";
   })
 });
 
 $(document).on('click', '.delete', function () {
   deleteTask($(this).data('id'), function () {
-    $('#new-task-content').empty();
     indexTasks(injectTasksIntoDom);
   });
 });
